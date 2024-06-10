74be71262baa6e29a228f6cacbc9b7e7cad6a936
MikaelaJ <mikaela_johansson@hotmail.com>
2024-06-10 11:48:26 +0200

    style(footer): Add some styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Footer () {
     return (
-        <footer className="container flex max-x-4xl">
-            <div className="container">
-            <p className="text-black-qred">&copy; 2024 Qred. All rights reserved.</p>
+        <footer className=" bg-black-qred-lighter flex w-full py-20 mt-14">
+            <div className="flex justify-center items-center w-full">
+            <p className="text-black-qred text-center">&copy; 2024 Qred. All rights reserved.</p>
             </div>
         </footer>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center p-24 w-full">
-      <div className="flex ">
-        <h1 className="pb-20">Välkommen till Qred</h1>
+    <main className="flex flex-col items-center min-h-screen w-full mt-40">
+      <div className="flex">
+        <h1 className="pb-20">Welcome to Qred</h1>
       </div>
       <div className="container flex-col space-y-5 justify-center w-1/5">
         <button className="flex bg-primary-qred w-full py-2 rounded-full justify-center text-white">
           <Link href="/profile">Go to Profile</Link>
         </button>
         <button className="flex bg-primary-qred w-full py-2 rounded-full justify-center text-white">
-          <Link href="/admin">Login</Link>
+          <Link href="/admin">Admin</Link>
         </button>
       </div>
     </main>
