1a718df8abdf1dd1b20006ec76fa39ea2079aa26
MikaelaJ <mikaela_johansson@hotmail.com>
2024-06-10 10:49:12 +0200

    feat: Add dynamic route to Profile page

--- app/components/admin/UserList.tsx
+++ app/components/admin/UserList.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { UsersApiResponse } from "@/app/models/interface";
+import { ApiUser } from "@/app/models/interface";
 import { useGetAllUsersQuery } from "@/lib/features/users/usersApiSlice";
-
-
+import Link from "next/link";
 
 export const UserList = () => {
   const { data, isError, isLoading, isSuccess } =
@@ -29,9 +28,13 @@ export const UserList = () => {
       <div className="flex justify-center">       
         {
           <div className="flex-col py-12 w-1/2 ">
-            {data.map((user: UsersApiResponse, index:number) => (
+            {data.map((user: ApiUser, index:number) => (
               <div className={`${index & 1 ? "bg-black-qred-lighter" : "bg-white"} py-2 px-2`} key={index}>
-              <h1 className="text-secondary-qred text-2xl font-bold">{user.name}</h1> 
+               <h1 className="text-secondary-qred text-2xl font-bold">
+                <Link href={`/profile/${user.id}`}>
+                  {user.name}
+                </Link>
+              </h1> 
               <p className="text-sm"><span className="font-bold">User name: </span>{user.username}</p>
               <p className="text-sm"><span className="font-bold">Email: </span> {user.email}</p>
               
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,14 +1,14 @@
 "use client"
-import { usePathname } from 'next/navigation'
-import { Profile } from "../components/profile/Profile";
 
+import { Profile } from "@/app/components/profile/Profile";
 
 export default function ProfilePage() {
+    
   return (
     <div className="flex flex-col container max-w-6xl justify-center">
-      <h1 className="flex text-center justify-center py-16">Welcome to your profile page</h1>
+      <h1 className="flex text-center justify-center py-16">Your profile page</h1>
       <section className="flex justify-center">
-      <Profile />
+      <Profile userId={1}/>
       </section>
     </div>
   );
